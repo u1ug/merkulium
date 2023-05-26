16bd066549eb0b3b518eb8551f5d44ad79cfebed
u1ug <maxim2006722@gmail.com>
2023-05-26 20:04:57 +0300

    Add random bytes generation

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,10 +1,24 @@
 package utils
 
-import "encoding/binary"
+import (
+	"crypto/rand"
+	"encoding/binary"
+)
 
 func Uint64ToBytes(num uint64) []byte {
 	bytes := make([]byte, 8)
 
 	binary.BigEndian.PutUint64(bytes, 8)
 	return bytes
+}
+
+func RandomBytes(length uint) []byte {
+	randomSequence := make([]byte, length)
+
+	_, err := rand.Read(randomSequence)
+	if err != nil {
+		return nil
+	}
+
+	return randomSequence
 }
